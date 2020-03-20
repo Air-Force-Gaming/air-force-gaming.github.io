2bda5e0ebdf1808a699578d7a23f88486b6afe17
Matthew Reichardt <me@matthewreichardt.com>
2020-03-20 22:51:09 +0900

    Combined news/events into one carousel sectionl; Added: gunfight and 24hr stream forms

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('.modal').modal();
+    $('.carousel').carousel();
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
+    setInterval(function() {
+        $('.carousel.carousel-slider').carousel('next');
+    }, 6000);
+    
+    
 });
